5b5b7e967ebaaa67e7e33a452551d7985a264168
Phong Huynh <phuynh@architech.ca>
2016-09-13 09:18:44 -0400

    Improve date between functionality

--- src/actions/MediaActions.js
+++ src/actions/MediaActions.js
@@ -39,15 +39,21 @@ function fetchDataBetweenDates(url, dispatch, collection) {
     return fetchJsonp(url, {timeout: 10000})
     .then(response => response.json())
     .then(response => {
+      let continueRecursion = true;
+
       let itemsWithinDateRange = response.data.filter((item) => {
         let createdTime = parseInt(item.created_time) * 1000;
+
+        if (startTime >= createdTime) {
+          continueRecursion = false;
+        }
 
         return createdTime >= startTime && createdTime <= endTime;
       });
 
       mediaItems = mediaItems.concat(itemsWithinDateRange);
 
-      if (response.pagination.next_url) {
+      if (response.pagination.next_url && continueRecursion) {
         getDataBetweenDatesRecursive(response.pagination.next_url);
       }
       else {
